db4304651f5d7488ab44f64e438939b67788c428
tdefosse <thomas.defosse@insa-rouen.fr>
2019-01-30 09:11:36 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -352,5 +352,166 @@ namespace bitbot
     {
         pins.servoWritePin(AnalogPin.P15, degrees);
     }
+// LED Blocks
 
+    // create a neopixel strip if not got one already. Default to brightness 40
+    function neo(): neopixel.Strip
+    {
+        if (!neoStrip)
+        {
+            neoStrip = neopixel.create(DigitalPin.P13, 12, NeoPixelMode.RGB);
+            neoStrip.setBrightness(40);
+        }
+        return neoStrip;
+    }
+
+    // update LEDs if _updateMode set to Auto
+    function updateLEDs(): void
+    {
+        if (_updateMode == BBMode.Auto)
+            neo().show();
+    }
+
+    /**
+      * Show LED changes
+      */
+    //% blockId="bitbot_neo_show" block="show LED changes"
+    //% weight=100
+    //% subcategory=Leds
+    export function neoShow(): void
+    {
+        neo().show();
+    }
+
+    /**
+      * Sets all LEDs to a given color (range 0-255 for r, g, b).
+      * @param rgb RGB color of the LED
+      */
+    //% blockId="bitbot_neo_set_color" block="set all LEDs to %rgb=bb_colours"
+    //% weight=95
+    //% subcategory=Leds
+    export function neoSetColor(rgb: number)
+    {
+        neo().showColor(rgb);
+        updateLEDs();
+    }
+
+    /**
+      * Clear all leds.
+      */
+    //% blockId="bitbot_neo_clear" block="clear all LEDs"
+    //% weight=90
+    //% subcategory=Leds
+    export function neoClear(): void
+    {
+        neo().clear();
+        updateLEDs();
+    }
+
+    /**
+     * Set LED to a given color (range 0-255 for r, g, b).
+     *
+     * @param ledId position of the LED (0 to 11)
+     * @param rgb RGB color of the LED
+     */
+    //% blockId="bitbot_neo_set_pixel_color" block="set LED at %ledId|to %rgb=bb_colours"
+    //% weight=85
+    //% subcategory=Leds
+    export function neoSetPixelColor(ledId: number, rgb: number): void
+    {
+        neo().setPixelColor(ledId, rgb);
+        updateLEDs();
+    }
+
+    /**
+      * Shows a rainbow pattern on all LEDs.
+      */
+    //% blockId="bitbot_neo_rainbow" block="set led rainbow"
+    //% weight=80
+    //% subcategory=Leds
+    export function neoRainbow(): void
+    {
+        neo().showRainbow(1, 360);
+        updateLEDs()
+    }
+
+    /**
+     * Rotate LEDs forward.
+     */
+    //% blockId="bitbot_neo_rotate" block="rotate LEDs"
+    //% weight=75
+    //% subcategory=Leds
+    export function neoRotate(): void
+    {
+        neo().rotate(1);
+        updateLEDs()
+    }
+
+    /**
+     * Shift LEDs forward and clear with zeros.
+     */
+    //% blockId="bitbot_neo_shift" block="shift LEDs"
+    //% weight=70
+    //% subcategory=Leds
+    export function neoShift(): void
+    {
+        neo().shift(1);
+        updateLEDs()
+    }
+
+    // Advanced blocks
+
+    /**
+      * Set LED update mode (Manual or Automatic)
+      * @param updateMode setting automatic will show LED changes automatically
+      */
+    //% blockId="bitbot_set_updateMode" block="set %updateMode|update mode"
+    //% weight=65
+    //% advanced=true
+    export function setUpdateMode(updateMode: BBMode): void
+    {
+        _updateMode = updateMode;
+    }
+
+    /**
+     * Set the brightness of the LEDs
+     * @param brightness a measure of LED brightness in 0-255. eg: 40
+     */
+    //% blockId="bitbot_neo_brightness" block="set LED brightness %brightness"
+    //% brightness.min=0 brightness.max=255
+    //% weight=60
+    //% advanced=true
+    export function neoBrightness(brightness: number): void
+    {
+        neo().setBrightness(brightness);
+        updateLEDs();
+    }
+
+    /**
+      * Get numeric value of colour
+      *
+      * @param color Standard RGB Led Colours
+      */
+    //% blockId="bb_colours" block=%color
+    //% weight=55
+    //% advanced=true
+    export function BBColours(color: BBColors): number
+    {
+        return color;
+    }
+
+    /**
+      * Convert from RGB values to colour number
+      *
+      * @param red Red value of the LED (0 to 255)
+      * @param green Green value of the LED (0 to 255)
+      * @param blue Blue value of the LED (0 to 255)
+      */
+    //% blockId="bitbot_convertRGB" block="convert from red %red| green %green| blue %blue"
+    //% weight=50
+    //% advanced=true
+    export function convertRGB(r: number, g: number, b: number): number
+    {
+        return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
+    }
 }
